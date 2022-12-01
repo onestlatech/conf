cb9ebc7a2a73c466cdea55b7d617f983fd3b62b3
Kévin Dunglas <kevin@dunglas.fr>
2022-12-01 23:54:45 +0100

    remove Created by Vercel

--- components/footer.tsx
+++ components/footer.tsx
@@ -37,11 +37,11 @@ export default function Footer() {
   return (
     <footer className={cn(styles.footer)}>
       <div className={styles['footer-legal']}>
-        <div className={styles['footer-hostedby']}>
+        {/*<div className={styles['footer-hostedby']}>
           <HostedByVercel />
           <div className={styles['footer-separator']} />
         </div>
-        {/*<div className={styles['footer-copyright']}>
+        <div className={styles['footer-copyright']}>
           Copyright © {`${new Date().getFullYear()} `} {COPYRIGHT_HOLDER || `${SITE_NAME}.`} All
           rights reserved.
         </div>*/}
